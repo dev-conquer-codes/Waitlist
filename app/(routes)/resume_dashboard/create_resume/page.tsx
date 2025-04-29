8eb2e307fc3e064a84cbbf9529211b05babe1804
dev-conquer-codes <rakshit2871@gmail.com>
2025-04-29 23:10:43 +0530

    2904252310

--- app/(routes)/resume_dashboard/create_resume/page.tsx
+++ app/(routes)/resume_dashboard/create_resume/page.tsx
@@ -1,26 +1,22 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import ResumeFinal from '@/ResumeWebsite/pages/ResumeFinal';
 
-
-
-
-
-export default function CreateResume() {
+function CreateResumeContent() {
   const searchParams = useSearchParams();
   const id = searchParams.get('id');
 
+  return <ResumeFinal id={id ?? ''} />;
+}
+
+export default function CreateResume() {
   return (
     <div>
-      {/* {id === '1' && <Resume1 />}
-      {id === '2' && <Resume2 />}
-      {!['1', '2'].includes(id ?? '') && (
-        <p className="text-center text-gray-500 mt-4">Invalid template selected.</p>
-      )} */}
-     <ResumeFinal id={id ?? ''} />
-
+      <Suspense fallback={<div>Loading...</div>}>
+        <CreateResumeContent />
+      </Suspense>
     </div>
   );
 }
